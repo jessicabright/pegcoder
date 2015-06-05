28ad0cdd19fafff99ff1e0d20247671c57ffda3a
gitolite <gitolite@cctv.org>
2015-06-05 17:13:07 +0000

    Fixed double looping problem; Added status update to json file; Moved and added a lot of stuff that needed cleaning up changed; Jessica Bright

--- libpegcoder.py
+++ libpegcoder.py
@@ -9,38 +9,39 @@ import os, sys, io, subprocess, json, shlex, fnmatch
 ###########
 def encode(settings, encodefile, infilename):
 	outfilename = os.path.basename(infilename)
-	#Setup the command
 	encodejsonpathd = settings['susrpath'] + "encoders/" + encodefile
 	myjson = json.loads(open(encodejsonpathd).read())
 	encodecmd = settings['encodecmd'] + " -i " + infilename
 	optlib = json.loads(open(settings['susrpath'] + "optlibs/" + myjson['encoder'] + ".json").read())
 	for opt in optlib:
 		encodecmd = encodecmd + " " + optlib[opt] + " " + myjson[opt]
-	#The scale option is ugly, so we'll add it here
 	encodecmd = encodecmd + " -vf scale=" + myjson['scale']
-	encodecmd = encodecmd + " " + settings['workspace'] + outfilename
+	if myjson['deinterlace'] == "True":
+		encodecmd = encodecmd + " -deinterlace"
+	encodecmd = encodecmd + " " + settings['workspace'] + settings['targettype'] + "-" + outfilename
 	#print encodecmd  #make this a proper debug,  or log level info?
-	#Run it here, while cleaning things up with shlex first
 	subprocess.check_call(shlex.split(encodecmd))
 	#check on status of ffmpeg or file or mediainfo and return something like 'encoded'?
 	#return (something)
 
 #get status from exsisting status json
-def getstatus(settings, jsonstatls):
+def getstatus(settings):
+	jsonstatls = []
 	for mysvarpath, vardirs, varfile in os.walk(settings['svarpath']):
                 for varfile in fnmatch.filter(varfile, settings['jfilext']):
                         jsonstatls.append(json.loads(open(os.path.join(mysvarpath, varfile)).read()))
         return jsonstatls
 
 #get list of all current mp4 files
-def getcurfiles(settings, curfilels):
+def getcurfiles(settings):
+	curfilels = []
 	for rootpath, dirnames, filenames in os.walk(settings['achpath']):
 		for curfile in fnmatch.filter(filenames, settings['fileext']):
 			pathfile = os.path.join(rootpath, curfile)
 			curfilels.append(pathfile)
 	return curfilels
 
-#update status json files
+#create json files that don't exsist already
 def updtjsls(settings, jsonstatls, curfilels):
 	cflag = "True"
 	mytemplate = json.loads(open(settings['susrpath'] + "templates/status-template.json").read())
@@ -53,19 +54,26 @@ def updtjsls(settings, jsonstatls, curfilels):
 			mynewjson['file'] = filename
 			mypath, myfilename = os.path.split(filename)
 			newpathf = settings['svarpath'] + myfilename + ".json"
+			mynewjson['json'] = newpathf
 			with open(newpathf, 'w') as myoutfile:
 				json.dump(mynewjson, myoutfile)
 		cflag = "True"
 
 #encode files based on orginizational settings using jsonstatls
-def chkupencode(settings, jsonstatls, org):
+def chkupencode(settings, jsonstatls):
 	for jsonobj in jsonstatls:
 		infilename = jsonobj['file']
 		if jsonobj['status-playback'] == "pending":
-			infilename = jsonobj['file']
-			encodefile = org + ".pb." + jsonobj['encoder-playback'] + ".json"
+			settings['targettype'] = "playback"
+			encodefile = "pb." + jsonobj['encoder-playback'] + ".json"
 			encode(settings, encodefile, infilename)
+			jsonobj['status-playback'] = "processed"
+			with open(jsonobj['json'], 'w') as myoutfile:
+				json.dump(jsonobj, myoutfile)
 		if jsonobj['status-vod'] == "pending":
-			outfilename = org + ".vod." + jsonobj['file']
-			encodefile = org + ".vod." + jsonobj['encoder-vod'] + ".json"
+			settings['targettype'] = "ondemand"
+			encodefile = "vod." + jsonobj['encoder-vod'] + ".json"
 			encode(settings, encodefile, infilename)
+			jsonobj['status-vod'] = "processed"
+			with open(jsonobj['json'], 'w') as myoutfile:
+				json.dump(jsonobj, myoutfile)
